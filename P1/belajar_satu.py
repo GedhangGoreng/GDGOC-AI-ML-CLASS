8ebecc929276c460da2d63f459e3b42348afe68a
GedhangGoreng <suryarajendraali2@gmail.com>
2026-03-08 10:57:49 +0700

    upload rekapan

---
+++ P1/belajar_satu.py
@@ -0,0 +1,44 @@
+#string int float boolean
+
+tempat_makan = "warteg" #string
+tinggi_badan = 178 #int
+tinggi_badan2 = 178.0 #float
+saya_keren = True # boolean
+
+print (tempat_makan)
+print (tinggi_badan)
+print (saya_keren)
+
+
+#2. data types
+
+#list
+buah_buahan = ["jeruk","apel","mangga"] #ordered, have index, searchable, changeable
+
+#random list
+random_list = ["ferrari", "mazda", 99, 99.0, True]
+
+#tuple
+soccer_player = ("messi","ronaldo","ishowspeed") #ordered, have index, searchable, unchangeable
+
+#set
+countries = ("england","france","mongol","usa") #unordered, doesnt have index, unsearchable, unchangeable
+
+#dictionary (key-value pairs) #ordered, have index, searchable, changeable
+#single dict
+data_anak_sekolah = {
+    "name" : "jek",
+    "kelas" : 12,
+    "jurusan" :"IPA"
+    }
+#multiple dict
+data_weekly_class = [
+    {
+    "name": "Rian",
+    "age": 19,
+    "group": 8},
+    {
+    "name": "Joko",
+    "age": 21,
+    "group": 10}
+]
